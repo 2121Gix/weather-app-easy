5f2e66f321265ddfd3d271736d57643c0ccc4955
Giacomo Casagranda <105413843+21Gix@users.noreply.github.com>
2023-04-15 20:58:16 +0200

    minor css adjust

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [weather, setWeather] = useState({});
+  const [loc, setLoc] = useState("");
   return (
     <div className="App">
       <div className='container'>
-        <input type='text' className='location' placeholder='Insert Location'></input>
+        <input type='text' className='location' placeholder='Insert Location' onChange={e => setLoc(e.currentTarget.value)}></input>
         <button class='searchbtn'><i class="fa fa-search" aria-hidden="true"></i></button>
         <div className='notfound'>
           <p>Location Not found</p>
         </div>
-        <div className='wDataPrimary'>
-          <p className='infoLocation'>Trento, Trentino-Alto Adige</p>
-          <p className='infoTemperature'>20 °C</p>
-          <p className='infoDesc'>Overcast</p>
-          <div className='wDataSecondary'>
-            <div>
-              <p className='infoFeelsLike'>25 °C</p>
-              Feels Like
-            </div>         
-            <div>
-              <p className='infoHumidity'>25 </p>
-              Humidity              
-            </div> 
-            <div>
-              <p className='infoWindSpeed'>25 kph</p>
-              Wind Speed
-            </div> 
-          </div>
+           <div className='wDataPrimary'>
+              <p className='infoTemperature'>20 °C</p>
+              <p className='infoDesc'>Overcast</p>
+              <div className='wDataSecondary'>
+                <div>
+                  <p className='infoFeelsLike'>25 °C</p>
+                  Feels Like
+                </div>         
+                <div>
+                  <p className='infoHumidity'>25 </p>
+                  Humidity              
+                </div> 
+                <div>
+                  <p className='infoWindSpeed'>25 kph</p>
+                  Wind Speed
+                </div> 
+              </div>
+            </div>
         </div>
-      </div>
     </div>
   );
 }
