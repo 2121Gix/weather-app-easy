494736299d413f5aeee4782656798c24f9a71845
Giacomo Casagranda <105413843+21Gix@users.noreply.github.com>
2023-04-16 11:00:33 +0200

    Only Icons Left

--- src/App.js
+++ src/App.js
@@ -1,35 +1,65 @@
 import './App.css';
 import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [weather, setWeather] = useState({});
+  const [weather, setWeather] = useState(null);
   const [loc, setLoc] = useState("");
+  const notFound = document.querySelector('.notfound');
+  const dataPr = document.querySelector('.wDataPrimary');
+  const dataSec = document.querySelector('.wDataSecondary');
+
+  const fetchWeather = () => {
+    const url1 = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
+    const url2 = '?unitGroup=metric&key=2RB9ML2JW4GSLRHGE9FL3TDJD&contentType=json';
+
+    axios.get(url1+loc+url2).then(res => {
+      setWeather(res.data);
+      notFound.style.display = "none";
+      dataPr.style.display = "flex";
+      dataSec.style.display = "flex";
+    })
+    .catch(err => {
+      notFound.style.display = "block";
+      dataPr.style.display = "none";
+      dataSec.style.display = "none";
+    })
+  }
   return (
     <div className="App">
       <div className='container'>
         <input type='text' className='location' placeholder='Insert Location' onChange={e => setLoc(e.currentTarget.value)}></input>
-        <button class='searchbtn'><i class="fa fa-search" aria-hidden="true"></i></button>
+        <button className='searchbtn' onClick={fetchWeather}><i className="fa fa-search" aria-hidden="true"></i></button>
         <div className='notfound'>
           <p>Location Not found</p>
         </div>
-           <div className='wDataPrimary'>
-              <p className='infoTemperature'>20 °C</p>
-              <p className='infoDesc'>Overcast</p>
-              <div className='wDataSecondary'>
-                <div>
-                  <p className='infoFeelsLike'>25 °C</p>
-                  Feels Like
-                </div>         
-                <div>
-                  <p className='infoHumidity'>25 </p>
-                  Humidity              
-                </div> 
-                <div>
-                  <p className='infoWindSpeed'>25 kph</p>
-                  Wind Speed
-                </div> 
-              </div>
+        <div className='wDataPrimary'>
+          {weather && <p className='infoLocation'>{weather?.resolvedAddress}</p>  }
+          {weather && <p className='infoTemperature'><b>{weather?.currentConditions.temp} °C</b></p> }
+          {weather && <p className='infoDesc'><i>{weather?.currentConditions.conditions}</i></p> }
+          <div className='wDataSecondary'>
+            {weather &&
+            <div>
+              <p className='infoFeelsLike'>{weather.currentConditions.feelslike } °C</p>
+              Feels Like
             </div>
+            }  
+            
+            {weather &&
+            <div>
+              <p className='infoHumidity'>{weather.currentConditions.humidity} </p>
+             Humidity              
+            </div> 
+            }
+
+            {weather &&
+             <div>
+               <p className='infoWindSpeed'>{weather.currentConditions.windspeed} kph</p>
+               Wind Speed
+            </div> 
+            }
+           </div>
+         </div>
         </div>
     </div>
   );
